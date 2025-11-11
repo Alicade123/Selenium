946f92601b31100a72d25bf1795fc9927c5fbc1c
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-11 20:30:46 +0200

    The 1st Optional Independence  Exercise on chapter 3: WedDriver

--- src/test/java/base/OptionalIndependentExercise1_3.java
+++ src/test/java/base/OptionalIndependentExercise1_3.java
@@ -1,4 +1,31 @@
 package base;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
 public class OptionalIndependentExercise1_3 {
+    WebDriver driver;
+
+    public void checkOut(){
+        System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        driver.findElement(By.linkText("Shifting Content")).click();
+        driver.findElement(By.linkText("Example 1: Menu Element")).click();
+        List<WebElement> list =driver.findElements(By.tagName("li"));
+        System.out.println(list.size());
+       driver.manage().window().setPosition(new Point(500, 100));
+        driver.quit();
+
+
+    }
+     public static void main(String[] args){
+         OptionalIndependentExercise1_3 testX = new OptionalIndependentExercise1_3();
+         testX.checkOut();
+     }
 }
