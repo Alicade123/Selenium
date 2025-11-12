f12a1eaad788dca7a323c71013a79483f5cd7c36
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-12 21:58:22 +0200

    the new Changes made on the base Class of the TestAutomation program

--- src/test/java/base/Chapter3FindWebElements.java
+++ src/test/java/base/Chapter3FindWebElements.java
@@ -1,6 +1,7 @@
 package base;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,19 +9,25 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.List;
 
 public class Chapter3FindWebElements {
-    WebDriver myWebDriver;
+    WebDriver driver;
 
     public void findWebElements(){
         System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
-        myWebDriver = new ChromeDriver();
-        myWebDriver.get("https://the-internet.herokuapp.com/");
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
 
-        List<WebElement> list = myWebDriver.findElements(By.tagName("a"));
+        List<WebElement> list = driver.findElements(By.tagName("a"));
         System.out.println(list.size());
 
-        myWebDriver.findElement(By.linkText("Inputs")).click();
-        myWebDriver.manage().window().maximize();
-        myWebDriver.close();
+        driver.findElement(By.linkText("Shifting Content")).click();
+        driver.findElement(By.linkText("Example 1: Menu Element")).click();
+        List<WebElement> items = driver.findElements(By.tagName("li"));
+        System.out.println(items.size());
+
+        driver.manage().window().maximize();
+//      driver.manage().window().fullscreen();
+//      driver.manage().window().setSize(new Dimension(375, 812));
+        driver.close();
 
 
     }
