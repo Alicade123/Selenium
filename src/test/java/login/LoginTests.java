581a5394df67412de1c1eab4d85e2775d41348b8
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-12 22:02:36 +0200

    Creating and writting the code to test the login functionality in the it's specific class

--- src/test/java/login/LoginTests.java
+++ src/test/java/login/LoginTests.java
@@ -1,4 +1,25 @@
 package login;
 
-public class LoginTests {
+import base.BaseTests;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import pages.SecureArea;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class LoginTests extends BaseTests {
+
+@Test
+public void testSuccessfulLogin(){
+     LoginPage loginPage = homePage.ClickAuthenticationLink();
+     loginPage.setUserName("tomsmith");
+     loginPage.setPassword("SuperSecretPassword!");
+     SecureArea secureArea = loginPage.clickLoginButton();
+
+     String actualResult = secureArea.getAlert();
+     //assertEquals(actualResult,"You logged into a secure area!","Your logged in successfully");
+     assertTrue(secureArea.getAlert().contains("You logged into a secure area!"),"Login Successful!");
+}
+
 }
