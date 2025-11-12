61996852c3e5130ea457bf0ea554e7837cd2134d
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-12 22:01:47 +0200

    Creating the dropdown class to test the dropdown functionality

--- src/test/java/dropdown/DropDownTests.java
+++ src/test/java/dropdown/DropDownTests.java
@@ -1,4 +1,20 @@
 package dropdown;
 
-public class DropDownTests {
+import base.BaseTests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DropDownTests extends BaseTests {
+
+    @Test
+    public  void testSelectOption(){
+        var dropDownPage = homePage.ClickDropDown();
+        String option = "Option 1";
+        dropDownPage.selectFromDropDown(option);
+        var selectedOption = dropDownPage.getSelectedOptions();
+        Assert.assertEquals(selectedOption.size(), 1);
+        Assert.assertTrue(selectedOption.contains(option), "Option 1 is not selected");
+
+    }
+
 }
