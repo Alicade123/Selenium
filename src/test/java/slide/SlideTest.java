50f1f0c18d6234fb945fcd3488a4a472f33cae04
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-15 18:24:04 +0200

    The Selenium learning progress with elements interactions -SLIDEBAR

--- src/test/java/slide/SlideTest.java
+++ src/test/java/slide/SlideTest.java
@@ -1,4 +1,14 @@
 package slide;
 
-public class SlideTest {
+import base.BaseTests;
+import org.testng.annotations.Test;
+import pages.HorizontalSlider;
+
+public class SlideTest  extends BaseTests {
+    @Test
+    public void testSlideBar(){
+        HorizontalSlider horizontalSlider = homePage.clickHorizontalSlider();
+        horizontalSlider.SlideRight( 15);
+
+    }
 }
