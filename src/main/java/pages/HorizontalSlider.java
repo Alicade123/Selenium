125eae6615478d3b7b3fb1104dd817eafc2c4eae
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-15 18:23:10 +0200

    The Selenium learning progress with elements interactions

--- src/main/java/pages/HorizontalSlider.java
+++ src/main/java/pages/HorizontalSlider.java
@@ -1,4 +1,28 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import static javax.management.Query.value;
+
 public class HorizontalSlider {
+    WebDriver driver;
+    private By slideBar = By.cssSelector(".sliderContainer input");
+    private By sliderValue = By.id("range");
+
+    public HorizontalSlider(WebDriver driver){
+        this.driver = driver;
+    }
+    public void SlideRight(int value){
+//        driver.findElement(slideBar).sendKeys(Keys.ARROW_RIGHT);
+        WebElement slider = driver.findElement(slideBar);
+        for (int i = 0; i < value; i++) {
+            slider.sendKeys(Keys.ARROW_RIGHT);
+        }
+
+    }
+
+
 }
