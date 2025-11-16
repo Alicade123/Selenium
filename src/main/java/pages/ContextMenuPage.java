a067f674d9562d7f2c1183063131fb23125b1b89
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-16 23:33:06 +0200

    The new test case wrotten to test the right click

---
+++ src/main/java/pages/ContextMenuPage.java
@@ -0,0 +1,30 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class ContextMenuPage {
+    private WebDriver driver;
+    private By hot_spotBox = By.id("hot-spot");
+    public ContextMenuPage(WebDriver  driver)    {
+        this.driver = driver;
+    }
+    public void RightClickInBox(){
+        Actions actions = new Actions(driver);
+        actions.contextClick(driver.findElement(hot_spotBox)).perform();
+    }
+    public String getAlertText(){
+        return driver.switchTo().alert().getText();
+    }
+
+    public void getAlertAccept(){
+        driver.switchTo().alert().accept();
+    }
+    /*
+
+
+
+     */
+}
