f8c3779e3c7fc161749e6bfae2bda4e3a178459c
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-16 23:28:25 +0200

    The new change made on AletTest case

--- src/test/java/alert/AlertTests.java
+++ src/test/java/alert/AlertTests.java
@@ -4,6 +4,9 @@ import base.BaseTests;
 import org.testng.annotations.Test;
 import pages.JavaScriptAlertsPage;
 
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
 public class AlertTests extends BaseTests {
     @Test
     public void testAcceptAlert(){
@@ -11,6 +14,25 @@ public class AlertTests extends BaseTests {
         JavaScriptAlertsPage javaScriptAlertsPage = homePage.clickJavaScriptAlerts();
         javaScriptAlertsPage.triggerAlert();
         javaScriptAlertsPage.getAlert();
-
+        String actualResults = javaScriptAlertsPage.getStatusResult();
+        assertEquals(actualResults,"You successfully clicked an alert" ,"Something went wrong");
+    }
+    @Test
+    public void testDismissAlert(){
+        JavaScriptAlertsPage javaScriptAlertsPage = homePage.clickJavaScriptAlerts();
+         javaScriptAlertsPage.triggerAlertButtonDismiss();
+         String text = javaScriptAlertsPage.getAlertTestDismuss();
+         javaScriptAlertsPage.getAlertDismiss();
+         assertEquals(text,"I am a JS Confirm", "Something went wrong");
+    }
+    @Test
+    public void testPromptAlert(){
+        JavaScriptAlertsPage javaScriptAlertsPage = homePage.clickJavaScriptAlerts();
+        javaScriptAlertsPage.clickTriggerAlertButtonPrompt();
+        String txt = "Alicade made this test";
+        javaScriptAlertsPage.setValue(txt);
+        javaScriptAlertsPage.getAlertPrompt();
+        String actualResults = javaScriptAlertsPage.getStatusResult();
+        assertEquals(actualResults,"You entered: "+txt ,"Something went wrong");
     }
 }
