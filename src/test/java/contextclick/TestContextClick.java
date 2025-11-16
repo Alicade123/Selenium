604fd76066c9e74405d58e6ac48bc7f0c79e8eb9
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-16 23:27:20 +0200

    The java new test about the right click test case

--- src/test/java/contextclick/TestContextClick.java
+++ src/test/java/contextclick/TestContextClick.java
@@ -1,4 +1,20 @@
 package contextclick;
 
-public class TestContextClick {
+import base.BaseTests;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class TestContextClick extends BaseTests {
+    @Test
+    public void contextClickTests(){
+        var contextMenuPage =  homePage.clickContextMenuLink();
+        contextMenuPage.RightClickInBox();
+        String txt = contextMenuPage.getAlertText();
+        contextMenuPage.getAlertAccept();
+        assertEquals(txt, "You selected a context menu", "Something went wrong");
+    }
+    /*
+
+   */
 }
