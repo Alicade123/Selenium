d899922b429d1326ee53ad51a049ff0ae7341d11
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-16 23:24:07 +0200

    New test case about the java file upload test case selenium and java

--- src/main/java/pages/FileUploadedPage.java
+++ src/main/java/pages/FileUploadedPage.java
@@ -1,4 +1,16 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class FileUploadedPage {
+    WebDriver driver;
+    private By status = By.id("uploaded-files");
+    public FileUploadedPage(WebDriver driver){
+        this.driver = driver;
+    }
+    public String getStatus(){
+        return driver.findElement(status).getText();
+    }
+
 }
