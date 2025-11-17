0773481199b0bc4cbe7463629e8d9ad30c9cca16
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-17 22:05:37 +0200

    Learning iframe selenium 3

--- src/main/java/pages/FramePage.java
+++ src/main/java/pages/FramePage.java
@@ -1,4 +1,17 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class FramePage {
+    WebDriver driver;
+    private By nestedFramesLink = By.linkText("Nested Frames");
+    public FramePage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public NestedFramesPage  clickFrames(){
+        driver.findElement(nestedFramesLink).click();
+        return new NestedFramesPage(driver);
+    }
 }
