7c8142df28f4dc4b09176b11aeb531e38a43de2b
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-17 22:05:19 +0200

    Learning iframe selenium 2

---
+++ src/main/java/pages/NestedFramesPage.java
@@ -0,0 +1,35 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class NestedFramesPage {
+    WebDriver driver;
+    private By frameLeft = By.name("frame-left");
+     String left_frame= "frame-left";
+    private By button_frame = By.name("frame-bottom");
+    NestedFramesPage(WebDriver driver){
+        this.driver = driver;
+    }
+    public void SwitchToLeftFrame(){
+        driver.switchTo().frame(left_frame);
+    }
+    public void SwitchToMainFrame(){
+        driver.switchTo().defaultContent();
+    }
+    public void getLeftFrameText(){
+        SwitchToLeftFrame();
+        SwitchToMainFrame();
+
+    }
+    public void getBottomFrameText(){
+        SwitchToLeftFrame();
+        SwitchToMainFrame();
+    }
+
+    /*
+
+
+     */
+
+}
