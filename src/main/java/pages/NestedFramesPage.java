bb88c9f5b596c3bf1624fb55adf4267318b21aee
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-18 16:35:50 +0200

    Finishing the optional independant exercise that deal with frames (nested frames) using the selenium and java

--- src/main/java/pages/NestedFramesPage.java
+++ src/main/java/pages/NestedFramesPage.java
@@ -5,33 +5,40 @@ import org.openqa.selenium.WebDriver;
 
 public class NestedFramesPage {
     WebDriver driver;
-    private By frameLeft = By.name("frame-left");
-     String left_frame= "frame-left";
-    private By button_frame = By.name("frame-bottom");
+
+
+     String top_Frame ="frame-top";
+    String left_Frame= "frame-left";
+    String buttom_Frame ="frame-bottom";
+    String middle_Frame ="frameset-middle";
+
+
+        private By topFrame =By.name("frame-top");
+        private By middleFrame =By.name("frameset-middle");
+        private By leftFrame =By.name("frame-left");
+        private By bottomFrame =By.name("frame-bottom");
+        private By body = By.tagName("body");
+
     NestedFramesPage(WebDriver driver){
         this.driver = driver;
     }
-    public void SwitchToLeftFrame(){
-        driver.switchTo().frame(left_frame);
-    }
-    public void SwitchToMainFrame(){
-        driver.switchTo().defaultContent();
-    }
-    public String getLeftFrameText(){
-        SwitchToLeftFrame();
-        SwitchToMainFrame();
-        return "";
+
+
+        public String getLeftFrameText(){
+            driver.switchTo().frame(top_Frame);
+            driver.switchTo().frame(left_Frame);
+            String text = driver.findElement(body).getText();
+            driver.switchTo().parentFrame(); //exit left to top
+            driver.switchTo().parentFrame(); //exit top to main
+            return text;
+
 
     }
     public String getBottomFrameText(){
-        SwitchToLeftFrame();
-        SwitchToMainFrame();
-        return "";
+        driver.switchTo().frame(driver.findElement(bottomFrame));
+        String txt= driver.findElement(body).getText();
+        driver.switchTo().defaultContent();
+        return txt;
     }
-
-    /*
-
-
-     */
 
 }
