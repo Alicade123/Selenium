bf60825f75708f0a67257e084dc46cd23c95c496
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-19 22:54:13 +0200

    The new change i made in framework to test the waiter/loading

--- src/main/java/pages/DynamicLoadingPage.java
+++ src/main/java/pages/DynamicLoadingPage.java
@@ -8,6 +8,7 @@ public class DynamicLoadingPage {
 //    String linkXpathPattern = ".//a[contains(text(), '%s')";
 //    private By link_Example1 = By.xpath(String.format(linkXpathPattern,"Example 1"));
     private By link_Example1 = By.linkText("Example 1: Element on page that is hidden");
+    private By link_Example2 = By.linkText("Example 2: Element rendered after the fact");
     public DynamicLoadingPage(WebDriver driver) {
         this.driver = driver;
     }
@@ -15,4 +16,9 @@ public class DynamicLoadingPage {
         driver.findElement(link_Example1).click();
         return new DynamicLoadingExample1Page(driver);
     }
+    public  DynamicLoadingExample2Page clickDynamicLoadingExample2Page(){
+        driver.findElement(link_Example2).click();
+        return new DynamicLoadingExample2Page(driver);
+    }
+
 }
