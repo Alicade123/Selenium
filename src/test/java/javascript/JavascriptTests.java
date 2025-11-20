cdde451938f0a36f76c74068f267922cadf26705
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-20 21:35:39 +0200

    Adding the new JavaScript package and new Class into test to test the scroll in webpage dom elements

--- src/test/java/javascript/JavascriptTests.java
+++ src/test/java/javascript/JavascriptTests.java
@@ -1,4 +1,14 @@
 package javascript;
+import base.BaseTests;
+import org.testng.annotations.Test;
+import pages.LargeDeepDOMPage;
 
-public class JavascriptTests {
+public class JavascriptTests extends BaseTests {
+    @Test
+    public void javascriptScrollDownTest(){
+        LargeDeepDOMPage largeDeepDOMPage = homePage.clickLargeDeepDOMLink();
+        largeDeepDOMPage.scrollToTable();
+    }
+
+
 }
