f5e0ec0b54211b203f4301ec72c568c0dbe608fa
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-20 21:18:04 +0200

    Creating the class in the framework to execute the scroll functionality in the page element

---
+++ src/main/java/pages/LargeDeepDOMPage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class LargeDeepDOMPage {
+    private WebDriver driver;
+
+    public LargeDeepDOMPage(WebDriver driver) {
+        this.driver =driver;
+    }
+
+}
