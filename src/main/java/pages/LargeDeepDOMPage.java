11ab6a0ed1b4058fe8164e31dc6b58fc207a0d1a
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-20 21:34:40 +0200

    Completing the scrolling method using the JavascriptExecutor to scroll down to the table

--- src/main/java/pages/LargeDeepDOMPage.java
+++ src/main/java/pages/LargeDeepDOMPage.java
@@ -1,12 +1,20 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class LargeDeepDOMPage {
     private WebDriver driver;
-
+    private By largeTableElement = By.id("large-table");
     public LargeDeepDOMPage(WebDriver driver) {
         this.driver =driver;
+    }
+    public void scrollToTable(){
+        WebElement largeTable = driver.findElement(largeTableElement);
+        String script = "arguments[0].scrollIntoView();";
+        ((JavascriptExecutor)driver).executeScript(script, largeTable);
     }
 
 }
