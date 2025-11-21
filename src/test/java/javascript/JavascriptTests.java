b4e9069e19331670d9d206149213231c83f88f0c
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-21 17:11:53 +0200

    Adding the file of testing the dropdown in the test JsExecutor while selecting multiple options from dropdown

--- src/test/java/javascript/JavascriptTests.java
+++ src/test/java/javascript/JavascriptTests.java
@@ -3,6 +3,11 @@ import base.BaseTests;
 import org.testng.annotations.Test;
 import pages.LargeDeepDOMPage;
 
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
 public class JavascriptTests extends BaseTests {
     @Test
     public void javascriptScrollDownTest(){
@@ -14,5 +19,17 @@ public class JavascriptTests extends BaseTests {
         homePage.clickInfiniteScrollLink().scrollToParagraph(10);
     }
 
+@Test
+    public void javascriptClickDropDownTest(){
 
+    var dropDownPage = homePage. clickDropDown4JsExecutorUse();
+    dropDownPage.addMultipleAttribute();
+
+    var optionsToSelect = List.of("Option 1", "Option 2");
+    optionsToSelect.forEach(dropDownPage::selectFromDropDown);
+
+    var selectedOptions = dropDownPage.getSelectedOptions();
+    assertTrue(selectedOptions.containsAll(optionsToSelect), "All options were not selected");
+    assertEquals(selectedOptions.size(), optionsToSelect.size(), "Number of selected items");
+}
 }
