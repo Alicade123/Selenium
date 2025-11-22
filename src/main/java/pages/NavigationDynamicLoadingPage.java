63b90214791ca336e154bc48173ddcde35482c74
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-23 00:01:06 +0200

    This is the testing file containing the testing methods

---
+++ src/main/java/pages/NavigationDynamicLoadingPage.java
@@ -0,0 +1,31 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import java.security.Key;
+
+public class NavigationDynamicLoadingPage {
+    WebDriver driver;
+
+    private By link_Example2 = By.linkText("Example 2: Element rendered after the fact");
+    public NavigationDynamicLoadingPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public NewWindowDynamicLoadingExample2Page clickDynamicLoadingExample2Page(){
+        Actions actions = new Actions(driver);
+        WebElement target = driver.findElement(link_Example2);
+        actions.keyDown(Keys.CONTROL).click(target).keyUp(Keys.CONTROL).perform();
+      return new NewWindowDynamicLoadingExample2Page(driver);
+    }
+ public void switchWindow(String newTab){
+        driver.switchTo().window(newTab);
+
+ }
+
+
+}
