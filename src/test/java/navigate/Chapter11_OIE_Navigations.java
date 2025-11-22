09de3a61ce283176be67f0b7afd2927671c052a0
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-23 00:25:33 +0200

    The change added in the page of test to test the navigation functionality in the website

--- src/test/java/navigate/Chapter11_OIE_Navigations.java
+++ src/test/java/navigate/Chapter11_OIE_Navigations.java
@@ -10,8 +10,9 @@ public class Chapter11_OIE_Navigations extends BaseTests {
  @Test
     public void testNavigations(){
      var dynamicPage2 = homePage.clickDynamicLoadingLink2();
-     dynamicPage2.switchWindow("The Internet");
      var newWindow = dynamicPage2.clickDynamicLoadingExample2Page();
+
+     dynamicPage2.switchWindow("https://the-internet.herokuapp.com/dynamic_loading/2");
     boolean actualResult= newWindow.isButtonExist();
      assertEquals(actualResult, true, "Arcade you messed up something");
 
