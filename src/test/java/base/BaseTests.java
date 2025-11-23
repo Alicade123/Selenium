90ce51d0e29a228b2a669fd5e1a18d4842508c19
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-23 21:33:27 +0200

    The addition of the after method annotation to be able to take the screenshot in the we

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,13 +1,20 @@
 package base;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 import pages.HomePage;
 import utils.WindowManager;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
@@ -45,11 +52,28 @@ public class BaseTests {
 //    }
     @AfterClass
     public void tearDown(){
-        //driver.quit();  //closes all the driver's session
+        driver.quit();  //closes all the driver's session
         //driver.close(); //closes only the current tab (window)
     }
     public WindowManager getWebDriver(){
         return new WindowManager(driver);
     }
+
+    @AfterClass
+    public void takeScreenShot(ITestResult result){
+        if(ITestResult.FAILURE == result.getStatus()){
+            var camera = (TakesScreenshot)driver;
+            File screenShoot = camera.getScreenshotAs(OutputType.FILE);
+            System.out.println("The absolute path of the screenshot is: " +screenShoot.getAbsolutePath());
+            try {
+                Files.move(screenShoot.toPath(),new File("resources/screenshot/Test.png").toPath());
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+
+    }
+
 }
 
