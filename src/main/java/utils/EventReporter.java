c45097b643f2c77f1dbe8d31ef112e616628bee5
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-24 15:22:56 +0200

    New class in the framework that implement some methods to report any action taken

---
+++ src/main/java/utils/EventReporter.java
@@ -0,0 +1,27 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.events.WebDriverListener;
+
+public class EventReporter implements WebDriverListener {
+    @Override
+    public void beforeClick(WebElement element) {
+        System.out.println("Clicking element: " + element);
+    }
+
+    @Override
+    public void afterClick(WebElement element) {
+//        System.out.println("Clicked element: " + element);
+    }
+
+    @Override
+    public void beforeGet(WebDriver driver, String url) {
+//        System.out.println("Navigating to: " + url);
+    }
+
+    @Override
+    public void afterGet(WebDriver driver, String url) {
+//        System.out.println("Finished navigating to: " + url);
+    }
+}
