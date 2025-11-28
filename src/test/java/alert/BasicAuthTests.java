f918d38455184c315eef1b2a8b2408f50ce7058a
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-28 19:58:24 +0200

    The attempt to test the basicAut login

--- src/test/java/alert/BasicAuthTests.java
+++ src/test/java/alert/BasicAuthTests.java
@@ -1,4 +1,15 @@
 package alert;
 
-public class BasicAuthTests {
+import base.BaseTests;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+
+public class BasicAuthTests extends BaseTests {
+@Test
+    public void testBasicAuthLogin(){
+    String actualResult = homePage.clickBasicAuth().getPageStatus();
+    assertEquals(actualResult, "Basic Auth", "You meesed up things Arcade!");
+}
 }
