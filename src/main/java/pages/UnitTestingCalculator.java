9ca4fd2910cef3090ef2b852d669429e84d9c0e0
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-12-04 22:01:49 +0200

    I added the method overloading there to test another unit/module

--- src/main/java/pages/UnitTestingCalculator.java
+++ src/main/java/pages/UnitTestingCalculator.java
@@ -4,4 +4,7 @@ public class UnitTestingCalculator {
     public int addition(int a, int b){
         return a+b;
     }
+    public int addition(int a, int b,int c){
+        return a+b+c;
+    }
 }
