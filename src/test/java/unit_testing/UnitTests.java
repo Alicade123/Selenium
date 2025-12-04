c04228ddad38dce22bf1bb052d9fdc544cf83356
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-12-04 22:02:18 +0200

    Call the created method overloading to test that module of overloaded method

--- src/test/java/unit_testing/UnitTests.java
+++ src/test/java/unit_testing/UnitTests.java
@@ -11,7 +11,9 @@ public class UnitTests {
     public void testCalcular(){
         unitTestingCalculator = new UnitTestingCalculator();
         int actualResult =unitTestingCalculator.addition(10,12);
-        System.out.println(actualResult); // 22
+        int actualResultOverloading = unitTestingCalculator.addition(100,500,300);
+        System.out.println(actualResult +" "+ actualResultOverloading); // 22
         assertEquals(actualResult, 22, "Something went Wrong");
+        assertEquals(actualResultOverloading,900,"Something went wrong");
     }
 }
